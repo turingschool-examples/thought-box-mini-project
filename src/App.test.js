108818d368218b004b2526318141a159207f7545
Christie Lynam <christielynam@gmail.com>
2018-06-28 12:30:46 -0600

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -8,8 +8,7 @@ it('matches the snapshot', () => {
 it('initial state is an empty array', () => {
 });
 
-it('having 2 thoughts in state should mean there are two thoughtCards', () => {
+it('createThought adds a thought to state', () => {
 });
 
-it('user can add a new thought which adds it to state and is also displayed on the screen', () => {
-});
+// Feel free to add additional tests. These are meant just to get you started.
